e2086894b41e55b5009b3099048936a62ba439f1
Matheus de Quadros <matheus.quadros@catolicasc.org.br>
2020-01-29 13:06:35 -0300

    starting the challenge

---
+++ digit-recognizer.py
@@ -0,0 +1,22 @@
+#%%
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+%matplotlib inline
+
+print('hi')
+
+
+# %%
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+
+# %%
+
+train.head()
+
+# %%
